af545026f789bb77474b4388ad213945a0ea111e
axaniit <137208435+axaniit@users.noreply.github.com>
2026-05-14 15:49:08 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,18 @@
-https://script.google.com/macros/s/AKfycbyoCFOUz_48NTyKsmlHX8ruKOVQ67DbwYvjOqimrTTRk7PBA8-dlnH_ZRvj-dU3ogsrtg/exec
+const CONFIG = {
+  apiUrl: "https://script.google.com/macros/s/AKfycbyoCFOUz_48NTyKsmlHX8ruKOVQ67DbwYvjOqimrTTRk7PBA8-dlnH_ZRvj-dU3ogsrtg/exec",
+
+  weekStartsOn: "monday",
+
+  subjects: [
+    {
+      id: "orgo",
+      name: "Orgo",
+      goalHours: 6
+    },
+    {
+      id: "micro",
+      name: "Micro",
+      goalHours: 10
+    }
+  ]
+};
